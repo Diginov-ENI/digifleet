88f0d661f253bfd89052b50226dfa53384e35ba7
MaxenceMillot <maxence.millot@hotmail.fr>
2021-04-17 16:53:56 +0200

    frontend - Ajout httpInterceptor pour transmission token csrf

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { UtilisateurListComponent } from './components/utilisateur/utilisateur-list.component';
 import { UtilisateurFormComponent } from './components/utilisateur/utilisateur-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -15,9 +17,12 @@ import { APP_ROUTING, DETAILS_ROUTES } from './app.routing';
 import { UtilisateurBackendService } from './backendservices/utilisateur.backendservice';
 import { VehiculeComponent } from './components/vehicule/vehicule.component';
 
+import { HttpXsrfInterceptor } from './http-interceptors/HttpXsrfInterceptor'
+
 @NgModule({
   declarations: [
     AppComponent,
+    UtilisateurListComponent,
     UtilisateurFormComponent,
     BandeauDigifleetComponent,
     DigifleetHomeComponent,
@@ -25,14 +30,21 @@ import { VehiculeComponent } from './components/vehicule/vehicule.component';
   ],
   entryComponents: [],
   imports: [
-    BrowserModule, 
+    BrowserModule,
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'csrftoken',
+      headerName: 'X-CSRFToken',
+    }),
     IonicModule.forRoot(), 
     BrowserAnimationsModule,
     MaterialModule,
     APP_ROUTING,
     RouterModule.forChild(DETAILS_ROUTES),
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpXsrfInterceptor, multi: true },
     UtilisateurBackendService,
   ],
   bootstrap: [AppComponent],
