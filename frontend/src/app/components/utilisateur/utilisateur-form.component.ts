ee081aa9796ca7b626ffbd2a76bdd3d654554131
Floue <claire.gaudet44@gmail.com>
2021-04-29 10:52:42 +0200

    Feature/df 10 + Feature/df 9 (#39)
    * Ajout du model Utilisateur et GestionnaireUtilisateur
    
    * Desactivation du module admin de base + mise en place du router
    
    * WIP - Mise en place permissions custom + views Utilisateur
    
    * ajout de /backend/migrations au .gitignore
    
    * WIP - Attention ne pas récupérer cette version mais s'en inspirer pour déclarer vos classes angular
    o Mise en place de l'architecture
    o Intégration de Angular material
    o Création d'un menu
    o [WIP] Mise ne place d'un système de routing
    
    * Mise en place architecture front/Angular material
    
    WIP - Attention ne pas récupérer cette version mais s'en inspirer pour déclarer vos classes angular
    
    o Mise en place de l'architecture
    o Intégration de Angular material
    o Création d'un menu
    o [WIP] Mise ne place d'un système de routing
    
    * backend - Ajout de la création d'un utilisteur
    
    * backend - Ajout de la modification PUT/PATCH d'un utilisateur + WIP delete
    
    * backend - API de CRUD utilisateur complet avec permissions d'accès
    
    * bugfix - Ajout d'un script pour installer material au lancement du container
    
    * Création du formulaire en cours
    
    * Merge/df9 with df10 (#31)
    
    * Update readme.md
    
    * Update readme.md
    
    * WIP - Attention ne pas récupérer cette version mais s'en inspirer pour déclarer vos classes angular
    o Mise en place de l'architecture
    o Intégration de Angular material
    o Création d'un menu
    o [WIP] Mise ne place d'un système de routing
    
    * bugfix - Ajout d'un script pour installer material au lancement du container
    
    Co-authored-by: Soac <claire.gaudet44@gmail.com>
    
    * Ajout des routes et du formulaire (GetParId & GetAll fonctionnels)
    
    * frontend - Ajout httpInterceptor pour transmission token csrf
    
    * frontend - getAll, getOneById and delete backend services
    
    * Modif dockerfiler
    
    * Correction d'un bug de données non à jour du getAll
    
    * Débuger python avec VSCode
    
    * Feature - Modifier un utilisateur
    
    * Ajout bouton modifier et ajouter + modale confirmation suppression
    
    * WIP tests unitaires
    
    * WIP unit tests
    
    * correctif merge DF-9/DF-10
    
    Co-authored-by: MaxenceMillot <maxence.millot@hotmail.fr>

---
+++ frontend/src/app/components/utilisateur/utilisateur-form.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, FormBuilder, ValidatorFn, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UtilisateurBackendService } from 'src/app/backendservices/utilisateur.backendservice';
+import { Utilisateur } from 'src/app/models/utilisateur';
+
+@Component({
+  selector: 'utilisateur-form',
+  templateUrl: 'utilisateur-form.component.html',
+})
+
+export class UtilisateurFormComponent implements OnInit {
+  utilisateur: Utilisateur;
+  form;
+
+  ProfilsUtilisateur = [{
+    id: 1,
+    valeur: 'Tous les droits'
+  },
+  {
+    id: 2,
+    valeur: 'Back office'
+  }
+  ]
+
+  constructor(private _utilisateurBackendService: UtilisateurBackendService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+  ) {
+    const utilisateurId = this.route.snapshot.paramMap.get('id');
+    if (utilisateurId)
+      this.chargerUtilisateur(utilisateurId);
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      Id: [''],
+      Username: ['', Validators.required],
+      Email: ['', [Validators.email, Validators.required]],
+      Nom: [''],
+      Prenom: [''],
+      Groups: ['']
+    });
+
+    this.form.controls.Username.updateValueAndValidity();
+    this.form.controls.Username.setValidators([Validators.required,
+    this.noWhitespaceValidator()]);
+    // unique(this.appelBack.utilisateurs$.value, ['LibLoginUse'], this.item.IdUse, ['IdUse'])]);
+    // ToDo validateur unique 
+
+    this.form.controls.Prenom.setValidators([Validators.required,
+    this.noWhitespaceStartEndValidator()]);
+
+    this.form.controls.Nom.setValidators([Validators.required,
+    this.noWhitespaceStartEndValidator()]);
+  }
+
+  sauver() {
+    this.utilisateur = new Utilisateur(this.form.value);
+
+    // ToDo factoriser le passage des champ vide à undefined
+    if (!this.utilisateur.Id) {
+      this.utilisateur.Id = undefined;
+      this.utilisateur.Groups = undefined;
+      this._utilisateurBackendService.addUtilisateur(this.utilisateur).subscribe(res => {
+        console.log(res);
+      });
+    } else {
+      let object:object = {
+        'Id' : this.utilisateur.Id,
+        'Username' : this.utilisateur.Username,
+        'Email' : this.utilisateur.Email,
+        'Nom' : this.utilisateur.Nom,
+        'Prenom' : this.utilisateur.Prenom
+      }
+
+      this._utilisateurBackendService.updateUtilisateur(object).subscribe(res => {
+        console.log(res);
+      });
+      // ToDo retour liste des utilisateurs
+    }
+  }
+
+  chargerUtilisateur(id) {
+    this._utilisateurBackendService.getUtilisateur(id).subscribe(res => {
+      this.utilisateur = new Utilisateur();
+      this.utilisateur = res;
+      this.itemToForm(this.utilisateur);
+    });
+  }
+
+  itemToForm(utilisateur: Utilisateur){
+    this.form.controls.Id.setValue(utilisateur.Id);
+    this.form.controls.Username.setValue(utilisateur.Username);
+    this.form.controls.Email.setValue(utilisateur.Email);
+    this.form.controls.Nom.setValue(utilisateur.Nom);
+    this.form.controls.Prenom.setValue(utilisateur.Prenom);
+  }
+
+  public noWhitespaceValidator(): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+      const isOnlyWhitespace = c.value ? c.value.includes(' ') : false;
+      const isValid = !isOnlyWhitespace;
+      return isValid ? null : { onlyWhitespace: true };
+    };
+  }
+
+  public noWhitespaceStartEndValidator(): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+      const isOnlyWhitespace = c.value ? c.value[0] === ' ' || c.value[c.value.length - 1] === ' ' : false;
+      const isValid = !isOnlyWhitespace;
+      return isValid ? null : { WhitespaceStartOrEnd: true };
+    };
+  }
+}
