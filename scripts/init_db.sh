140f7a2305d44d95eae34611ff2fa54842c3667b
MaxenceMillot <maxence.millot@hotmail.fr>
2021-09-03 11:56:41 +0200

    Update init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 # container must be running !
 docker exec -it docker_db_1 mkdir ../datasets
 docker cp ./backend/migrations/datasets/insert_users.sql docker_db_1:datasets/insert_users.sql
-docker cp ./backend/migrations/datasets/insert_users.sql docker_db_1:datasets/insert_groups.sql
+docker cp ./backend/migrations/datasets/insert_groups.sql docker_db_1:datasets/insert_groups.sql
 docker exec -t docker_db_1 psql -U postgres -d postgres -f ./datasets/insert_users.sql
 docker exec -t docker_db_1 psql -U postgres -d postgres -f ./datasets/insert_groups.sql
 
