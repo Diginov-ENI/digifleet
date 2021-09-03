7a2d030586c2ca651b7c55c52f41675dfb30ab0e
MaxenceMillot <maxence.millot@hotmail.fr>
2021-09-03 15:05:08 +0200

    Ajout scripts jeux de données

---
+++ backend/migrations/datasets/insert_all_demo.sql
@@ -0,0 +1,102 @@
+-- # INSERT USERS
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',true,'admin@admin.fr','admin','admin','admin',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'sartu@campus-eni.fr','sartu','ARTU','Sébastien',true, true);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'abaille@campus-eni.fr','abaille','BAILLE','Anne-Lise',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'lbouvet@campus-eni.fr','lbouvet','BOUVET','Laurent',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'gbrossier@campus-eni.fr','gbrossier','BROSSIER','Gilles',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'ochiron@campus-eni.fr','ochiron','CHIRON','Olivier',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'mclaveau@campus-eni.fr','mclaveau','CLAVEAU','Mathieu',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'fdelachesnais@campus-eni.fr','fdelachesnais','DELACHESNAIS','Frédéric',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'mdenis@campus-eni.fr','mdenis','DENIS','Mathieu',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'cfender@campus-eni.fr','cfender','FENDER','Clément',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'rgautier@campus-eni.fr','rgautier','GAUTIER','Romain',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'sgobin@campus-eni.fr','sgobin','GOBIN','Stéphane',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'fgrelier@campus-eni.fr','fgrelier','GRELIER','François',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'ygrohan@campus-eni.fr','ygrohan','GROHAN','Yann',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'tgroussard@campus-eni.fr','tgroussard','GROUSSARD','Thierry',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'tguyader@campus-eni.fr','tguyader','GUYADER','Thélo',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'tlargeau@campus-eni.fr','tlargeau','LARGEAU','Thierry',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'mle louarn@campus-eni.fr','mle louarn','LE LOUARN','Mathieu',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'sleboucher@campus-eni.fr','sleboucher','LEBOUCHER','Sébastien',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'amariault@campus-eni.fr','amariault','MARIAULT','Anthony',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'fmarsaudon@campus-eni.fr','fmarsaudon','MARSAUDON','François',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'gmichaud@campus-eni.fr','gmichaud','MICHAUD','Grégory',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'pmontembault@campus-eni.fr','pmontembault','MONTEMBAULT','Philippe',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'crichez@campus-eni.fr','crichez','RICHEZ','Cédric',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'rroussel@campus-eni.fr','rroussel','ROUSSEL','Romain',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'dsanchez@campus-eni.fr','dsanchez','SANCHEZ','Denis',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'sthomaslesne@campus-eni.fr','sthomaslesne','THOMAS-LESNE','Stéphane',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'fthouin@campus-eni.fr','fthouin','THOUIN','Frédéric',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'jtrillard@campus-eni.fr','jtrillard','TRILLARD','Julien',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'stropee@campus-eni.fr','stropee','TROPÉE','Sylvain',true, false);
+INSERT INTO backend_utilisateur (password,is_superuser,email,username,nom,prenom,is_active, is_password_to_change) VALUES ('pbkdf2_sha256$260000$4OZZQeCXRyT6lXq0FHNk0s$4qMuhLQ6n5Yz/3yADPIpU1Git61LNfXnEsUhT8p+h8w=',false,'fwawrzyniak@campus-eni.fr','fwawrzyniak','WAWRZYNIAK','François-Xavier',true, false);
+
+-- # INSERT GROUPS
+INSERT INTO auth_group(name)
+VALUES ('administration'),
+('backoffice'),
+('utilisateurs');
+
+-- # INSERT GROUPS PERMISSIONS
+INSERT INTO auth_group_permissions(group_id, permission_id)
+VALUES (1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 10),
+(1, 11),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 24),
+(1, 25),
+(1, 26),
+(1, 27),
+(1, 28),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(2, 12),
+(2, 13),
+(2, 14),
+(2, 15),
+(2, 16),
+(2, 17),
+(2, 18),
+(2, 19),
+(2, 20),
+(2, 21),
+(2, 22),
+(2, 23),
+(3, 18),
+(3, 19),
+(3, 20),
+(3, 21),
+(3, 22),
+(3, 23);
+
+-- # INSERT SITES
+INSERT INTO backend_site(name)
+VALUES ('ENI Nantes'),
+('ENI Rennes'),
+('ENI Niort'),
+('ENI Quimpert');
+
+-- # INSERT VEHICULES
+INSERT INTO backend_vehicule (immatriculation, modele, marque, couleur, nb_place, is_active, site_id)
+VALUES ('AV-229-CK', 'Mégane', 'Renault', 'blanc', '5', TRUE, 1),
+('RV-337-TS', '208', 'Peugeot', 'blanc', '5', TRUE, 1),
+('XP-381-KF', '208', 'Peugeot', 'blanc', '5', TRUE, 2);
+
+-- # INSERT EMPRUNTS
